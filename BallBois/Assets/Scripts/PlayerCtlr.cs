4048b6fdaee799ab74dfe180ec04e82af3245c71
Necropuppy <nfooteaz@gmail.com>
2018-07-14 19:16:40 -0400

    Initial File Upload

---
+++ BallBois/Assets/Scripts/PlayerCtlr.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCtlr : MonoBehaviour {
+
+    public float speed;
+    private int count;
+    private Rigidbody rb;
+    public Text countText;
+    public Text winText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        countText.text = "Cubes: " + count.ToString();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            countText.text = "Cubes: " + count.ToString();
+            if (count >= 5) {
+                winText.text = "YOU WIN";
+            }
+        }
+    }
+}
